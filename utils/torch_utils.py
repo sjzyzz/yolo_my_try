70c1fccb5a58b7b78715157596ac342c2e89252d
sjzyzz <yzz20@mails.tsinghua.edu.cn>
2021-02-28 16:56:19 +0800

    fix a bug in torch_utils.py

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -26,7 +26,7 @@ def select_device(device="", batch_size=None):
                 batch_size % n == 0
             ), f"batch-size {batch_size} not multiple of GPU count {n}"
         space = " " * len(s)
-        for i, d in enumerate(device.split(",")) if device else range(n):
+        for i, d in enumerate(device.split(",") if device else range(n)):
             p = torch.cuda.get_device_properties(i)
             s += f"{'' if i == 0 else space}CUDA:{d} ({p.name}, {p.total_memory / 1024 ** 2}MB)\n"
     else:
